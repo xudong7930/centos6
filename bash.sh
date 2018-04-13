225fa37aaab63e40c89ff16ce3b22b3e7595f34c
xudong7930 <xudong7930@126.com>
2018-04-13 19:18:55 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,6 @@
-kill -9 `ps -a | grep php71 | grep -v grep | awk '{print $1}'`
+
+> kill -9 `ps -a | grep php71 | grep -v grep | awk '{print $1}'`
+
+或者
+
+> ps -a | grep php71 | grep -v grep | awk '{print $1}' | xargs kill -9
